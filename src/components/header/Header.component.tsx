2e027b76460b248feb3a2f3d6f55326705a458a7
Katya Vakulenko <rogerosha@gmail.com>
2024-04-26 23:00:15 +0200

    add header for mobile version

--- src/components/header/Header.component.tsx
+++ src/components/header/Header.component.tsx
@@ -1,2 +1,15 @@
+import React from 'react';
+
 import styles from './header.module.css';
-export const HeaderComponent = () => <header className={styles.header}>Header</header>;
+
+export const HeaderComponent = () => (
+    <header className={styles['header']}>
+        <div className={styles['logo']}>
+            <img className={styles['MA-logo']} src="./MAlogo.svg" alt="MAlogo"></img>
+        </div>
+        <div className={styles['right-menu']}>
+            <img className={styles['cart']} src="./cart.svg" alt="cart"></img>
+            <img className={styles['menu']} src="./menu.svg" alt="menu"></img>
+        </div>
+    </header>
+);
