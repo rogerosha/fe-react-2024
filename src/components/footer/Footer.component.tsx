24d79f7af4282d9ed7aed89136a05537684114d0
Katya Vakulenko <rogerosha@gmail.com>
2024-05-01 00:43:09 +0200

    refactor component SocialIcon for footer

--- src/components/footer/Footer.component.tsx
+++ src/components/footer/Footer.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { SocialIcon } from '@/components/socialIcon/SocialIcon.component.tsx';
-import { MAlink } from '@/constants/social-links.ts';
+import { facebookLink, instagramLink, linkedinLink, MAlink } from '@/constants/social-links.ts';
 
 import styles from './footer.module.css';
 
@@ -10,7 +10,9 @@ export const FooterComponent = () => (
         <hr className={styles['line']} />
         <div className={styles['footer-content']}>
             <ul className={styles['social-links']}>
-                <SocialIcon icon={''} link={''}></SocialIcon>
+                <SocialIcon icon={'./facebook.svg'} link={facebookLink} />
+                <SocialIcon icon={'./linkedin.svg'} link={linkedinLink} />
+                <SocialIcon icon={'./instagram.svg'} link={instagramLink} />
             </ul>
             <div className={styles['footer-text']}>
                 <p>
