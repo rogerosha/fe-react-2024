9c9afb21e8f57ae78454e84f95dbe815c792da7d
Katya Vakulenko <rogerosha@gmail.com>
2024-05-12 23:07:09 +0200

    fix header width

--- src/components/header/Header.component.tsx
+++ src/components/header/Header.component.tsx
@@ -13,45 +13,47 @@ export interface HeaderProps {
 
 export const HeaderComponent: FC<HeaderProps> = ({ page, onPageClick, selectedProducts }) => (
     <header className={styles['header']}>
-        <div className={styles['left-part']}>
-            <div className={styles['logo']}>
-                <img className={styles['MA-logo']} src="./MAlogo.svg" alt="MAlogo"></img>
+        <div className={styles['header-wrapper']}>
+            <div className={styles['left-part']}>
+                <div className={styles['logo']}>
+                    <img className={styles['MA-logo']} src="./MAlogo.svg" alt="MAlogo"></img>
+                </div>
+                <div className={styles['day-night']}>
+                    <img className={styles['day-mode']} src="./daymode.png" alt="Sun" />
+                    <div className={styles['divider']}></div>
+                    <img className={styles['night-mode']} src="./nightmode.png" alt="Moon" />
+                </div>
             </div>
-            <div className={styles['day-night']}>
-                <img className={styles['day-mode']} src="./daymode.png" alt="Sun" />
-                <div className={styles['divider']}></div>
-                <img className={styles['night-mode']} src="./nightmode.png" alt="Moon" />
-            </div>
-        </div>
-        <div className={styles['right-part']}>
-            <div className={styles['navigation']}>
-                <span
-                    className={styles['navigation-link']}
-                    style={{ fontWeight: page === 'about' ? 'bold' : 'initial' }}
-                    onClick={() => onPageClick('about')}
-                >
-                    About
-                </span>
-                <span
-                    className={styles['navigation-link']}
-                    style={{ fontWeight: page === 'products' ? 'bold' : 'initial' }}
-                    onClick={() => onPageClick('products')}
-                >
-                    Products
-                </span>
-            </div>
-            <div className={styles['right-menu']}>
-                <img className={styles['cart']} src="./cart.svg" alt="cart"></img>
-                {selectedProducts.length}
-                <img className={styles['menu']} src="./menu.svg" alt="menu"></img>
-            </div>
-            <div className={styles['buttons']}>
-                <button className={styles['button-login']}>
-                    <img className={styles['login']} src="./login.png" alt="Login"></img> Login
-                </button>
-                <button className={styles['button-signup']}>
-                    <img className={styles['signup']} src="./signup.png" alt="Sign Up"></img> Sign Up
-                </button>
+            <div className={styles['right-part']}>
+                <div className={styles['navigation']}>
+                    <span
+                        className={styles['navigation-link']}
+                        style={{ fontWeight: page === 'about' ? 'bold' : 'initial' }}
+                        onClick={() => onPageClick('about')}
+                    >
+                        About
+                    </span>
+                    <span
+                        className={styles['navigation-link']}
+                        style={{ fontWeight: page === 'products' ? 'bold' : 'initial' }}
+                        onClick={() => onPageClick('products')}
+                    >
+                        Products
+                    </span>
+                </div>
+                <div className={styles['right-menu']}>
+                    <img className={styles['cart']} src="./cart.svg" alt="cart"></img>
+                    {selectedProducts.length}
+                    <img className={styles['menu']} src="./menu.svg" alt="menu"></img>
+                </div>
+                <div className={styles['buttons']}>
+                    <button className={styles['button-login']}>
+                        <img className={styles['login']} src="./login.png" alt="Login"></img> Login
+                    </button>
+                    <button className={styles['button-signup']}>
+                        <img className={styles['signup']} src="./signup.png" alt="Sign Up"></img> Sign Up
+                    </button>
+                </div>
             </div>
         </div>
     </header>
